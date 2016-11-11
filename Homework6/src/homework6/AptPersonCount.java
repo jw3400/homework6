1ee53c3784bdd773fd706bc825543317cd44b7c8
ChoiJinwon <jw3400@gmail.com>
2016-11-11 11:08:45 +0900

    소스코드 작성중[과제5번 수정중]

--- Homework6/src/homework6/AptPersonCount.java
+++ Homework6/src/homework6/AptPersonCount.java
@@ -13,39 +13,68 @@ public class AptPersonCount {
 		String apt [][] = new String [5][3];
 		
 		String input = null;
+		int total = 0;
 		
 		while (true) 
 		{
 			for(int i=0; i<apt.length; i++)
 			{
-				for(int j=0; j<apt[i].length; j++)
-				{
+				int j=0;
+				while(j<apt[i].length)
+				{				
 					System.out.print((i+1)+"0"+(j+1)+"호에 살고 있는 인원 수 :  ");
 					input = su.next();
+					apt[i][j] = input;
 					
 					if(!input.matches("^[0-9]*$"))		
 					{
-						System.out.println("양수만 입력가능합니다. 다시 입력하십시오");
+						System.out.println("양수만 입력가능합니다. 다시 입력하십시오");						
 						continue;
-					}
-					
-				}
+					}					
+					j++;			
+				}			
 			}
 			
-			int apt2 [][]= new int [5][3];
+			int [][] apt2 = new int[5][3];
+			int [] floor = new int [5];
+			int [] ho = new int [3];
+			for(int i=0; i<apt.length; i++)
+			{
+				for(int j=0; j<apt[i].length; j++)
+				{
+					apt2[i][j] = Integer.parseInt(apt[i][j]);
+					total += apt2[i][j];		
+				}
+			}		
+			System.out.println("===================================");
+			System.out.println("이 아파트의 거주자는 모두 " + total + "명입니다." );
+			
 			for(int i=0; i<apt2.length; i++)
 			{
 				for(int j=0; j<apt2[i].length; j++)
 				{
-					apt2[i][j] = Integer.parseInt(apt[i][j]);
-					System.out.print(apt2[i][j] + " ");
+					floor[i] += apt2[i][j];
+					ho[j] += apt2[i][j];
 				}
-				System.out.println(" ");
 			}
-
+			
+			System.out.println("===================================");			
+			for(int i=0; i<floor.length; i++)
+			{
+				System.out.println((i+1)+"층에 사는 거주자는 모두 " +floor[i]+"명 입니다." );
+			}
+			
+			System.out.println("===================================");			
+			for(int j=0; j<ho.length; j++)
+			{
+				System.out.println((j+1)+"호 라인에 사는 거주자는 모두 " +ho[j]+"명 입니다." );
+			}
+		
+			
 		su.close();
 		System.exit(0);
 		}
+		
 		
 	}
 }
--- Homework6/src/homework6/OnlyNumber.java
+++ Homework6/src/homework6/OnlyNumber.java
@@ -1,8 +1,68 @@
 package homework6;
 
+import java.util.*;
+
 public class OnlyNumber {
 
 	public void question5() {
+		
+		System.out.println("중복된 숫자 허용하지 않기");
+		System.out.println("=====================================");
+		System.out.println("1~100사이의 숫자를 입력하세요.");
+		
+		Scanner su = new Scanner(System.in);
+		int [] num = new int [10];
+		String input = null;
+		boolean check=false;
+		
+		int i = 1;
+		while(true)
+		{
+			if(i<10)
+			{
+				System.out.print(i+"번째 숫자 입력 : ");
+				input = su.nextLine();
+				i++;
+								
+				if(!input.matches("^[0-9]*$"))		
+				{
+					System.out.println("양수만 입력가능합니다. 다시 입력하십시오");						
+					continue;
+				}					
+			
+				if(Integer.parseInt(input)<1 || Integer.parseInt(input)>100)
+				{
+					System.out.println("1~100까지의 숫자만 입력할 수 있습니다. 다시 입력하십시오.");
+					continue;
+				}		
+			
+				num[i-1] = Integer.parseInt(input);
+			
+				for(int j=0; j<i-1; j++)
+				{
+					if(num[i-1] == num[j])
+					{
+						System.out.println("잘못 입력하셨습니다. 다시 입력하세요.");
+						check = true;
+						break;
+					}	
+				}
+				
+				if(check==false)
+					continue;
+				
+				
+			}
+		
+		
+		System.out.println("===================================");			
+			for(int j=9; j>=0; j--)
+			{
+				System.out.println((j+1)+" 번째 숫자는  " +num[j]+"입니다." );
+			}
+		su.close();	
+		System.exit(0);	
+		}		
 		
 	}
 }
